448721923452ae1600cf4da725ccaadc604ee5eb
Parnian-Shirvani <sh82.parnian@gmail.com>
2026-02-05 15:01:47 +0330

    Add basic data types with numeric boolean logic

---
+++ data_types.py
@@ -0,0 +1,17 @@
+# Different type of variables
+
+# String 
+name = input("What is your name?")
+print(type(name))
+
+# Integer number
+age = int(input("How old are you?"))
+print(type(age))
+
+# Float number
+height = float(input("How tall are you?"))
+print(type(height))
+
+# Boolean 
+is_student = 1 == int(input("Are you a student? (1=yes / 0=no)"))
+print(type(is_student))
