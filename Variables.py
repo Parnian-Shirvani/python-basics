fc4e019c448dca6fea3367126517bde58d55d8f2
Parnian-Shirvani <sh82.parnian@gmail.com>
2026-02-06 22:20:01 +0330

    Add variable naming rules and dynamic typing examples

---
+++ Variables.py
@@ -0,0 +1,19 @@
+# How should we name our variables?
+# you should not start your variable names with numbers but they can contain numbers anywhere else 
+# you should not use symbols such as : $, @, ^, *, ...
+# you can start your variable name with underline _ 
+# Python is sensitive to upper or lower case
+# in Python, snake_case is the most commonly used naming convention
+
+myName = "Parnian" # Camel case variable naming
+MyName = "Parnian" # Upper Camel case variable naming 
+myname = "Parnian" # Lower case variable naming
+my_name = "Parnian" # Snake_case variable naming
+PI = 3.14 # Constants
+
+# Python is a dynamically typed:
+# the type of a variable can change during execution
+
+myVariable = "Parnian" # string
+myVariable = 22 # integer
+myVariable = 159.5 # float
