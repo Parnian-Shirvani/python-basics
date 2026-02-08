1427af76018e9bedffe57efbe4db004049127e47
Parnian-Shirvani <sh82.parnian@gmail.com>
2026-02-08 23:11:25 +0330

    Add basic input and output examples

---
+++ input_output.py
@@ -0,0 +1,10 @@
+# Input and Output in Python :
+# Output : print() 
+# Input : input()
+
+# Input
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+# Output with variables
+print("Name = " , name , "| Age = " , age )
