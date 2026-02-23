c0048fac1b5d5bd62ab03a013a6059a2fb1943ec
Parnian-Shirvani <sh82.parnian@gmail.com>
2026-02-23 22:08:50 +0330

    Add examples of logical operators

---
+++ 11_logical_operators.py
@@ -0,0 +1,32 @@
+# logical operators
+
+# and
+
+print(f"True and True : {True and True}")
+print(f"True and False : {True and False}")
+print(f"False and True : {False and True}")
+print(f"False and False : {False and False}")
+
+print("________________________")
+
+# or 
+
+print(f"True or True : {True or True}")
+print(f"True or False : {True or False}")
+print(f"False or True : {False or True}")
+print(f"False or False : {False or False}")
+
+print("________________________")
+
+# not 
+
+print(f"not True : {not True}")
+print(f"not False : {not False}")
+
+print("________________________")
+
+# Real examples 
+print("for examples: ")
+print(f"5 > 3 and 10 > 2 : {5 > 3 and 10 > 2}")
+print(f"5 > 10 or 3 < 8 : {5 > 10 or 3 < 8}")
+print(f" not (4 == 4) : {4 == 4}")
