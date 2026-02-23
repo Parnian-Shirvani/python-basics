b36d0f4f8db5cc1f385796ae3955414433f9c73f
Parnian-Shirvani <sh82.parnian@gmail.com>
2026-02-23 16:47:22 +0330

    Add examples of comparison operators

---
+++ 10_comparison_operators.py
@@ -0,0 +1,21 @@
+# Comparison operators
+
+num_1 , num_2 = int(input("Enter 2 numbers : 1. ")) , int(input("2. "))
+
+# == 
+print(f"{num_1} == {num_2} : {num_1 == num_2}")
+
+# !=
+print(f"{num_1} != {num_2} : {num_1 != num_2}")
+
+# <
+print(f"{num_1} < {num_2} : {num_1 < num_2}")
+
+# <=
+print(f"{num_1} <= {num_2} : {num_1 <= num_2}")
+
+# >
+print(f"{num_1} > {num_2} : {num_1 > num_2}")
+
+# >=
+print(f"{num_1} >= {num_2} : {num_1 >= num_2}")
